98e5e896c3ed53455d50f3623b35d0965af74a75
wonjulee2003 <wonju2003@gmail.com>
2019-07-19 15:45:13 +0900

    timer

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import tkinter
 from tkinter import ttk
+import datetime
+
+
+def calculate_diff():
+    abc = datetime.datetime(year=int(year_entry.get()), month=int(month_entry.get()), day=int(day_entry.get()))
+    current_time = datetime.datetime.now()
+    difference = abc - current_time
+    left_label.configure(text=str(difference) + " left")
+    left_label.after(10, calculate_diff)
+
+
 
 root = tkinter.Tk()
 
@@ -22,11 +33,10 @@ month_label.grid(row=0, column=3, padx=5, pady=5)
 day_label = ttk.Label(main_frame, text="일 까지")
 day_label.grid(row=0, column=5, padx=5, pady=5)
 
-left_label = ttk.Label(main_frame, text="남았습니다")
-left_label.grid(row=1, column=5, padx=5, pady=5)
+left_label = ttk.Label(main_frame, text="")
+left_label.grid(row=1, column=0, padx=5, pady=5, columnspan=6)
 
-button = ttk.Button(main_frame, text="some text")
+button = ttk.Button(main_frame, text="some text", command=calculate_diff)
 button.grid(row=2, column=5, padx=5, pady=5)
-
 
 root.mainloop()
